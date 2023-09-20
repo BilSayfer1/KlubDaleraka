46d076e26bcff10dab60c668b001a08ce20b5675
BilSayfer1 <142802154+BilSayfer1@users.noreply.github.com>
2023-09-20 23:06:47 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+let name = prompt`Как вас зовут?`.trim()
+if (name.charAt(0).toLowerCase() === `a`) {
+    let age = +prompt`Сколько тебе лет братищка?`
+} else {
+    alert`Уходи от сюда собака`
+} if (age => 18 && age <=40 ) {
+
+} else {
+    alert`Уходи от сюда собака`
+} let kolwo = +prompt`Сколько вас человек?`
+if (kolwo > 0 && kolwo <= 10) { 
+
+} else {
+    alert`Уходи от сюда собака`
+} let money = +prompt`Сколько у тебя денюжек бумажных?`
+if (money => 100) {  
+} else {`Уходи от сюда собака`}
+let club = prompt`Добро пожаловать в клуб Нига`
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
